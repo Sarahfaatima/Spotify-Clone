d090525a33e5f64e80d3603f1b5f015692c7b8a8
Sarahfaatima <sarahfatima1924@gmail.com>
2025-01-12 23:42:59 +0530

    playbar added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Lets write JavaScript!")
+
+async function main(){
+
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML= response;
+    let tds = div.getElementsByTagName("td")
+    console.log(tds)
+}
+
+main()
